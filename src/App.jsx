e54b5d8bd11f43fbb27d7f3a8ae55ef230ed215c
Andy Cryan <acryan6@gatech.edu>
2020-10-15 18:27:22 -0600

    Create project tree, add background image

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.scss';
-import HomePage from './components/homepage';
+import HomePage from './pages/homepage/homepage.component';
 
 function App() {
   return (
