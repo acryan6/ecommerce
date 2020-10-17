f51fd5b7b576bd38d6ddf962d496d628f36e3f47
Andy Cryan <acryan6@gatech.edu>
2020-10-17 09:24:03 -0600

    Renders collection preview

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.scss';
-import HomePage from './pages/homepage/homepage.component';
+import HomePage from './pages/homepage/homepage.component.jsx';
+import ShopPage from './pages/shop/shop.component.jsx';
 import { Switch, Route } from 'react-router-dom';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path='/' component={HomePage}/>
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
