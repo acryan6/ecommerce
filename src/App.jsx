1ab5d9373194c56c598522929f5925ce9a9ab5eb
Andy Cryan <acryan6@gatech.edu>
2020-10-18 16:35:47 -0600

    Setup enzyme for tests

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.scss';
 import HomePage from './pages/homepage/homepage.component.jsx';
 import ShopPage from './pages/shop/shop.component.jsx';
 import { Switch, Route } from 'react-router-dom';
 import Header from './components/header/header.component.jsx';
-import SignInAndUpPage from './pages/sign-in-up/sign-in-up.component.jsx'
+import SignInAndUpPage from './pages/sign-in-up/sign-in-up.component.jsx';
+import { auth } from './firebase/firebase.utils';
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={HomePage}/>
-        <Route path='/shop' component={ShopPage} />
-        <Route path='/signin' component={SignInAndUpPage} />
-      </Switch>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  componentDidMount() {
+    auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+
+      console.log(user);
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={HomePage}/>
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInAndUpPage} />
+        </Switch>
+      </div>
+    )
+  }
 }
 
 export default App;
