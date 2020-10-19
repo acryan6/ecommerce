159c603118097908cc1786b319695f4f7b08659a
Andy Cryan <acryan6@gatech.edu>
2020-10-19 11:42:05 -0600

    Test for sign up component

---
+++ src/components/sign-up/sign-up.test.js
@@ -0,0 +1,18 @@
+import { shallow } from 'enzyme';
+import React from 'react';
+import SignUp from './sign-up.component.jsx';
+
+describe('sign-up component', () => {
+
+  it('expect to render sign-up component', () => {
+    expect(shallow(<SignUp />)).toMatchSnapshot();
+  });
+
+  it('correctly handles changes and submits', () => {
+    const wrapper = shallow(<SignUp />);
+
+  })
+
+})
+
+
