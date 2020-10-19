81e367704d7f472229a6c18a09528740d00dcf6c
Andy Cryan <acryan6@gatech.edu>
2020-10-18 20:57:38 -0600

    Logic for any auth changes

--- src/components/sign-in/sign-in.test.js
+++ src/components/sign-in/sign-in.test.js
@@ -2,6 +2,17 @@ import { shallow } from 'enzyme';
 import React from 'react';
 import SignIn from './sign-in.component.jsx';
 
-it('expect to render sign-in component', () => {
-  expect(shallow(<SignIn />)).toMatchSnapshot();
+describe('sign-in component', () => {
+
+  it('expect to render sign-in component', () => {
+    expect(shallow(<SignIn />)).toMatchSnapshot();
+  });
+
+  it('correctly handles changes and submits', () => {
+    const wrapper = shallow(<SignIn />);
+
+  })
+
 })
+
+
