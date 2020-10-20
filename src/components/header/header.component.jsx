4d52dfebcc1e8413851d1793f5c90b30e8e9fc97
Andy Cryan <acryan6@gatech.edu>
2020-10-20 12:55:00 -0600

    Add hidden state to header, toggles cart on click

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -7,7 +7,7 @@ import { connect } from 'react-redux';
 import CartIcon from '../cart-icon/cart-icon.component.jsx';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component.jsx';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className='header'>
     <Link className='logo-container' to='/'>
       <Logo className='logo'/>
@@ -26,12 +26,17 @@ const Header = ({ currentUser }) => (
       }
       <CartIcon />
     </div>
-    <CartDropdown />
+    {
+      hidden
+        ? null
+        : <CartDropdown />
+    }
   </div>
 );
 
-const mapStateToProps = (state) => ({
-  currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden }}) => ({
+  currentUser,
+  hidden
 })
 
 export default connect(mapStateToProps)(Header);
