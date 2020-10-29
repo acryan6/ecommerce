43c31472018e81899b84c6f0703e1f9bc8fd71d6
Andy Cryan <acryan6@gatech.edu>
2020-10-29 00:05:19 -0600

    Seperate CRA into client folder, added express server to process stripe payment

---
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51HfUMYJJ1bA9IYq4QhceIwBWOKlflSNjmelUWW1wS4S98iIDNOk0u7f4DHK8i1Deq50laKAZbx4ohNy7GYdWRvBF00kfq9uEbf";
+
+  const onToken = (token) => {
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token,
+      },
+    })
+      .then((res) => {
+        alert("Payment successful");
+      })
+      .catch((error) => {
+        console.log("Payment error: ", JSON.parse(error));
+        alert(
+          "There was an issue with your payment. Please use the provided credit card."
+        );
+      });
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="Royal Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
