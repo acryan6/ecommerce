904e45d815f2751d74f7cc263e20745327d4f4a6
Andy Cryan <acryan6@gatech.edu>
2020-11-01 11:40:05 -0700

    Add tests to cart-icon component

--- client/src/components/cart-icon/cart-icon.test.js
+++ client/src/components/cart-icon/cart-icon.test.js
@@ -6,4 +6,29 @@ import { CartIcon } from "./cart-icon.component";
 describe("CartIcon component", () => {
   let wrapper;
   let mockToggleCartHidden;
+
+  beforeEach(() => {
+    mockToggleCartHidden = jest.fn();
+
+    const mockProps = {
+      itemCount: 0,
+      toggleCartHidden: mockToggleCartHidden,
+    };
+
+    wrapper = shallow(<CartIcon {...mockProps} />);
+  });
+
+  it("should render CartIcon component", () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it("should call toggleCartHidden when icon is clicked", () => {
+    wrapper.find("CartIconContainer").simulate("click");
+    expect(mockToggleCartHidden).toHaveBeenCalled();
+  });
+
+  it("should render itemCount as text", () => {
+    const itemCount = parseInt(wrapper.find("ItemCountContainer").text());
+    expect(itemCount).toBe(0);
+  });
 });
