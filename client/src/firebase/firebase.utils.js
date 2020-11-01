f76f63c5e04ad14a0e54ebbe6f1b11b1ce7c4997
Andy Cryan <acryan6@gatech.edu>
2020-10-31 23:17:20 -0600

    Add cart methods and sagas to interact with firebase to get previous cart items upon signing in again

--- client/src/firebase/firebase.utils.js
+++ client/src/firebase/firebase.utils.js
@@ -1,6 +1,6 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
 
 const config = {
   apiKey: "AIzaSyBN485ztrAnCqkd0H31REXlmSRnMVPeLXI",
@@ -9,16 +9,18 @@ const config = {
   projectId: "ecommerce-db-6a065",
   storageBucket: "ecommerce-db-6a065.appspot.com",
   messagingSenderId: "687270434640",
-  appId: "1:687270434640:web:c5ff64c55f130afe5e7e55"
+  appId: "1:687270434640:web:c5ff64c55f130afe5e7e55",
 };
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
-  if (!userAuth) { return };
+  if (!userAuth) {
+    return;
+  }
 
   const userRef = firestore.doc(`users/${userAuth.uid}`);
   const snapShot = await userRef.get();
 
-  if(!snapShot.exists) {
+  if (!snapShot.exists) {
     const { displayName, email } = userAuth;
     const createdAt = new Date();
 
@@ -27,59 +29,73 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
         displayName,
         email,
         createdAt,
-        ...additionalData
-      })
+        ...additionalData,
+      });
     } catch (error) {
-      console.log('error creating user', error.message);
+      console.log("error creating user", error.message);
     }
   }
 
   return userRef;
 };
 
+export const getUserCartRef = async (userId) => {
+  const cartsRef = firestore.collection("carts").where("userId", "==", userId);
+  console.log(cartsRef);
+  const snapShot = await cartsRef.get();
+
+  if (snapShot.empty) {
+    const cartDocRef = firestore.collection("carts").doc();
+    await cartDocRef.set({ userId, cartItems: [] });
+    return cartDocRef;
+  } else {
+    return snapShot.docs[0].ref;
+  }
+};
+
 export const addCollectionAndDocs = async (collectionKey, objectsToAdd) => {
   const collectionRef = firestore.collection(collectionKey);
   const batch = firestore.batch();
-  objectsToAdd.forEach(obj => {
+  objectsToAdd.forEach((obj) => {
     const newDocRef = collectionRef.doc();
     batch.set(newDocRef, obj);
   });
 
-  return await batch.commit()
+  return await batch.commit();
 };
 
 export const convertCollectionsSnapshotToMap = (collections) => {
-  const transformedCollection = collections.docs.map(doc => {
+  const transformedCollection = collections.docs.map((doc) => {
     const { title, items } = doc.data();
     return {
       routeName: encodeURI(title.toLowerCase()),
       id: doc.id,
       title,
-      items
-    }
+      items,
+    };
   });
 
   return transformedCollection.reduce((accumulator, collection) => {
     accumulator[collection.title.toLowerCase()] = collection;
     return accumulator;
-  }, {})
-}
+  }, {});
+};
 
 firebase.initializeApp(config);
 
 export const getCurrentUser = () => {
   return new Promise((resolve, reject) => {
-    const unsubscribe = auth.onAuthStateChanged(userAuth => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
       unsubscribe();
       resolve(userAuth);
-    }, reject)
+    }, reject);
   });
-}
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 export const googleProvider = new firebase.auth.GoogleAuthProvider();
-googleProvider.setCustomParameters({ prompt: 'select_account' });
+googleProvider.setCustomParameters({ prompt: "select_account" });
 export const signInWithGoogle = () => auth.signInWithPopup(googleProvider);
 export default firebase;
--- client/src/redux/cart/cart.reducer.js
+++ client/src/redux/cart/cart.reducer.js
@@ -1,9 +1,9 @@
-import CartActionTypes from './cart.types';
-import { addItemToCart, removeItem, clearItemFromCart } from './cart.utils'
+import CartActionTypes from "./cart.types";
+import { addItemToCart, removeItem, clearItemFromCart } from "./cart.utils";
 
 const INITIAL_STATE = {
   hidden: true,
-  cartItems: []
+  cartItems: [],
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
@@ -11,31 +11,36 @@ const cartReducer = (state = INITIAL_STATE, action) => {
     case CartActionTypes.TOGGLE_CART_HIDDEN:
       return {
         ...state,
-        hidden: !state.hidden
-      }
+        hidden: !state.hidden,
+      };
     case CartActionTypes.ADD_ITEM:
       return {
         ...state,
-        cartItems: addItemToCart(state.cartItems, action.payload)
-      }
+        cartItems: addItemToCart(state.cartItems, action.payload),
+      };
     case CartActionTypes.REMOVE_ITEM:
       return {
         ...state,
-        cartItems: removeItem(state.cartItems, action.payload)
-      }
+        cartItems: removeItem(state.cartItems, action.payload),
+      };
     case CartActionTypes.CLEAR_ITEM_FROM_CART:
       return {
         ...state,
-        cartItems: clearItemFromCart(state.cartItems, action.payload)
-      }
+        cartItems: clearItemFromCart(state.cartItems, action.payload),
+      };
     case CartActionTypes.CLEAR_CART:
       return {
         ...state,
-        cartItems: []
-      }
+        cartItems: [],
+      };
+    case CartActionTypes.SET_CART_FROM_FIREBASE:
+      return {
+        ...state,
+        cartItems: action.payload,
+      };
     default:
       return state;
   }
-}
+};
 
 export default cartReducer;
