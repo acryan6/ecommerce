a6e773f39e57a30bbdcae847b8e95b285c88f20f
Andy Cryan <acryan6@gatech.edu>
2020-11-02 16:09:21 -0700

    Convert checkout-item to styled-component

---
+++ client/src/components/cart-item/cart-item.test.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { shallow } from "enzyme";
+
+import CartItem from "./cart-item.component";
+
+describe("CartItem component", () => {
+  // let wrapper = shallow(<CartItem {...mockProps} />);
+});
