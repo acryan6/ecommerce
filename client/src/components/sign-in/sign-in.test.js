504cdf4e122f06c1f5176c2621a91bcdb073fd27
Andy Cryan <acryan6@gatech.edu>
2020-11-09 12:56:24 -0700

    Add tests for sign-in component

--- client/src/components/sign-in/sign-in.test.js
+++ client/src/components/sign-in/sign-in.test.js
@@ -1,17 +1,39 @@
 import { shallow } from "enzyme";
 import React from "react";
 import { SignIn } from "./sign-in.component.jsx";
+import FormInput from "../form-input/form-input.component";
 
 describe("sign-in component", () => {
   let wrapper;
   let mockEmailSignInStart;
   let mockGoogleSignInStart;
 
-  it("expect to render sign-in component", () => {
-    expect(shallow(<SignIn />)).toMatchSnapshot();
+  beforeEach(() => {
+    mockEmailSignInStart = jest.fn();
+    mockGoogleSignInStart = jest.fn();
+
+    const mockProps = {
+      emailSignInStart: mockEmailSignInStart,
+      googleSignInStart: mockGoogleSignInStart,
+    };
+
+    wrapper = shallow(<SignIn {...mockProps} />);
   });
 
-  it("correctly handles changes and submits", () => {
-    const wrapper = shallow(<SignIn />);
+  it("should render sign-in component", () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it("correctly handles submit", () => {
+    const preventDefault = jest.fn();
+    wrapper.find("form").simulate("submit", { preventDefault });
+
+    expect(mockEmailSignInStart).toHaveBeenCalled();
+  });
+
+  it("calls googleSignInStart", () => {
+    wrapper.find("CustomButton").at(1).simulate("click");
+
+    expect(mockGoogleSignInStart).toHaveBeenCalled();
   });
 });
