91240052ce14e90d98184c2284a620503ec34314
Andy Cryan <acryan6@gatech.edu>
2020-11-09 13:06:46 -0700

    Add tests for sign-up component

--- client/src/components/sign-up/sign-up.test.js
+++ client/src/components/sign-up/sign-up.test.js
@@ -3,11 +3,26 @@ import React from "react";
 import { SignUp } from "./sign-up.component.jsx";
 
 describe("sign-up component", () => {
-  it("expect to render sign-up component", () => {
-    expect(shallow(<SignUp />)).toMatchSnapshot();
+  let wrapper;
+  let mockSignUpStart;
+
+  beforeEach(() => {
+    mockSignUpStart = jest.fn();
+
+    const mockProps = {
+      signUpStart: mockSignUpStart,
+    };
+
+    wrapper = shallow(<SignUp {...mockProps} />);
   });
 
-  it("correctly handles changes and submits", () => {
-    const wrapper = shallow(<SignUp />);
+  it("expect to render sign-up component", () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it("correctly handles submit", () => {
+    wrapper.find("form").simulate("submit", { preventDefault: () => {} });
+
+    expect(mockSignUpStart).toHaveBeenCalled();
   });
 });
