ffde7251e42377fd0c4f6fbcceaf717b46ea3315
Andy Cryan <acryan6@gatech.edu>
2020-11-09 10:47:55 -0700

    Changed form-input to styled-components

--- client/src/components/directory/directory.component.jsx
+++ client/src/components/directory/directory.component.jsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import './directory.styles.scss';
-import { connect } from 'react-redux';
-import MenuItem from '../menu-item/menu-item.component';
-import { selectDirectorySections } from '../../redux/directory/directory.selectors';
-import { createStructuredSelector } from 'reselect';
+import React from "react";
+import "./directory.styles.scss";
+import { connect } from "react-redux";
+import MenuItem from "../menu-item/menu-item.component";
+import { selectDirectorySections } from "../../redux/directory/directory.selectors";
+import { createStructuredSelector } from "reselect";
 
-const Directory = ({ sections }) => (
+export const Directory = ({ sections }) => (
   <div className="directory-menu">
     {sections.map(({ id, ...otherSectionProps }) => (
-      <MenuItem key={id} {...otherSectionProps}/>
+      <MenuItem key={id} {...otherSectionProps} />
     ))}
   </div>
 );
 
 const mapStateToProps = createStructuredSelector({
-  sections: selectDirectorySections
-})
+  sections: selectDirectorySections,
+});
 
 export default connect(mapStateToProps)(Directory);
--- client/src/components/directory/directory.test.js
+++ client/src/components/directory/directory.test.js
@@ -1,7 +1,7 @@
-import { shallow } from 'enzyme';
-import React from 'react';
-import Directory from './directory.component.jsx';
+import { shallow } from "enzyme";
+import React from "react";
+import { Directory } from "./directory.component.jsx";
 
-it('expect to render directory component', () => {
-  expect(shallow(<Directory />)).toMatchSnapshot();
-})
+it("expect to render directory component", () => {
+  expect(shallow(<Directory sections={[]} />)).toMatchSnapshot();
+});
